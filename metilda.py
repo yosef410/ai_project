bfa3cfbf856c1e6b05be1d3503a7b6226146aecb
lily <lily@lol>
2026-01-09 15:40:14 -0500

    first code on linex

--- metilda.py
+++ metilda.py
@@ -1,1 +1,10 @@
+hello = (input("hi how are you doing:"))
+def check(f):
+    if f == "good":
+       return "wow thats nice"
+    else:
+       return "it will be better"
+moral = check(hello)
+print(moral)
+print("\n")
 print("menora")
